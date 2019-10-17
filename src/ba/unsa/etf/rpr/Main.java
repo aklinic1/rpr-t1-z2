8a8b39545f628a24ea7667e727f9dbae02453b53
Amer Klinic <aklinic1@etf.unsa.ba>
2019-10-18 00:21:33 +0200

    final

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,28 @@
 package ba.unsa.etf.rpr;
+import java.util.Scanner;
 
 public class Main {
+    public static int SumaCifara(int broj) {
+        int suma=0;
+        while(broj!=0){
+            int temp = broj;
+            temp = temp%10;
+            suma += temp;
+            broj = broj/10;
+        }
+        return suma;
+    }
 
     public static void main(String[] args) {
-        int 
+
+        System.out.println("Unesite n: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i ;
+        System.out.println("Brojevi koji su djeljivi sumom svojih cifara su :");
+        for(i = 1; i <= n; i++)
+            if(i % SumaCifara(i) == 0) System.out.println(i + " ");
+
     }
 }
+
